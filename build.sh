d63cfb9f810e76a2d72cf9486cb6ad58d9257880
Vittore Zen <vittore@zen.pn.it>
2018-06-12 22:48:07 +0200

    `date`

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pushd /Users/vittore/Projects/www.zen.pn.it/jekyll_source
+rm -rf _site/*
 jekyll build
 pushd /Users/vittore/Projects/www.zen.pn.it/vittore.github.io
 rsync -avz --delete /Users/vittore/Projects/www.zen.pn.it/jekyll_source/_site/* /Users/vittore/Projects/www.zen.pn.it/vittore.github.io --exclude .git
