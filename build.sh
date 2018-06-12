01078facfb175de519f51635f5a468bbf42ef458
Vittore Zen <vittore@zen.pn.it>
2018-06-12 22:25:03 +0200

    first

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pushd /Users/vittore/Projects/www.zen.pn.it/jekyll_source
+jekyll build
+pushd /Users/vittore/Projects/www.zen.pn.it/vittore.github.io
+cp -r /Users/vittore/Projects/www.zen.pn.it/_site/* /Users/vittore/Projects/www.zen.pn.it/vittore.github.io
+git add .
+git commit -am '`date`'
+git push origin master
+popd
+popd
