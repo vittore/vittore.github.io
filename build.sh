b1a33ff6f78ef649ffc9b52d5542a79ee08e06f3
Vittore Zen <vittore@zen.pn.it>
2018-06-12 22:25:35 +0200

    `date`

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 pushd /Users/vittore/Projects/www.zen.pn.it/jekyll_source
 jekyll build
 pushd /Users/vittore/Projects/www.zen.pn.it/vittore.github.io
-cp -r /Users/vittore/Projects/www.zen.pn.it/_site/* /Users/vittore/Projects/www.zen.pn.it/vittore.github.io
+cp -r /Users/vittore/Projects/www.zen.pn.it/jekyll_source/_site/* /Users/vittore/Projects/www.zen.pn.it/vittore.github.io
 git add .
 git commit -am '`date`'
 git push origin master
