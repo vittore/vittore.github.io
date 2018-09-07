a4d4a47f617e03383efe4c72279353bb476be8f4
Vittore Zen <vittore@zen.pn.it>
2018-09-07 07:41:48 +0200

    Fri Sep  7 07:41:48 CEST 2018

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-jekyll build --source /Users/vittore/Projects/www.zen.pn.it/jekyll_source --destination /Users/vittore/Projects/www.zen.pn.it/vittore.github.io
+bundle exec jekyll build --source /Users/vittore/Projects/www.zen.pn.it/jekyll_source --destination /Users/vittore/Projects/www.zen.pn.it/vittore.github.io
 pushd /Users/vittore/Projects/www.zen.pn.it/vittore.github.io
 git add .
 git commit -am "`date`"
-git push origin master 
+git push origin master
 popd
